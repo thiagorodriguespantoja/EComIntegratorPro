09bf7b3adc291a30f8a5c2fc6d3a6502101ad1f4
Thiago Rodrigues Pantoja <thiago.pantoja@enkel.com.br>
2024-06-20 20:28:25 -0300

    Adicionar arquivos de projeto.

---
+++ E-Com Integrator/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿// /Controllers/ProductsController.cs
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var products = await _productService.GetAllProductsAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var product = await _productService.GetProductByIdAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(Product product)
+    {
+        await _productService.AddProductAsync(product);
+        return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, Product product)
+    {
+        if (id != product.Id)
+        {
+            return BadRequest();
+        }
+        await _productService.UpdateProductAsync(product);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _productService.DeleteProductAsync(id);
+        return NoContent();
+    }
+}
